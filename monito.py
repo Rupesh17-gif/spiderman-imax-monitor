74c09006aa6970ffe207cda6d312205d09892fe0
Rupesh17-gif <krishnarupesh4@gmail.com>
2026-07-27 08:29:17 +0530

    Refactor Telegram bot to use getMe endpoint

--- monito.py
+++ monito.py
@@ -1,18 +1,15 @@
 import requests
 import os
 
-BOT_TOKEN = os.getenv("8850541633:AAFyLTq27M1zQEP2E9TDeAqv0CkiLSmdL9M")
-CHAT_ID = os.getenv("729459307")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+print("BOT_TOKEN =", BOT_TOKEN)
+print("CHAT_ID =", CHAT_ID)
 
-r = requests.post(
-    url,
-    data={
-        "chat_id": CHAT_ID,
-        "text": "✅ GitHub Test"
-    }
-)
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/getMe"
+
+r = requests.get(url)
 
 print("Status:", r.status_code)
 print("Response:", r.text)
