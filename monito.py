18345879182de59d5109b4476b9bf71eee10dcb2
Rupesh17-gif <krishnarupesh4@gmail.com>
2026-07-27 08:55:39 +0530

    Simplify BOT_TOKEN logging in monito.py

--- monito.py
+++ monito.py
@@ -1,8 +1,5 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
 
-print("BOT_TOKEN Length:", len(BOT_TOKEN))
-print("BOT_TOKEN First 10 Chars:", BOT_TOKEN[:14])
-print("CHAT_ID:", CHAT_ID)
+print("TOKEN:", BOT_TOKEN[:15])
