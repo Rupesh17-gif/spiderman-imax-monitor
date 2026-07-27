9c136e2eab61e13e07573059b6955514f7f37223
Rupesh17-gif <krishnarupesh4@gmail.com>
2026-07-27 09:10:13 +0530

    Integrate Telegram API to fetch bot info
    Added functionality to fetch and display bot information from Telegram API.

--- monito.py
+++ monito.py
@@ -1,5 +1,13 @@
+import requests
 import os
 
-BOT_TOKEN = os.getenv**BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-print("TOKEN:", BOT_**KEN[:15])
+print("TOKEN:", BOT_TOKEN[:15])
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/getMe"
+
+r = requests.get(url)
+
+print("Status:", r.status_code)
+print("Response:", r.text)
