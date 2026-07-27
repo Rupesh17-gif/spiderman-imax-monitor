83f9470b7baa93bcbaeb82f51b5766df82bf5087
Rupesh17-gif <krishnarupesh4@gmail.com>
2026-07-27 08:57:56 +0530

    Integrate Telegram API to check bot status
    Added functionality to fetch and print bot status from Telegram API.

--- monito.py
+++ monito.py
@@ -1,5 +1,11 @@
+import requests
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-print("TOKEN:", BOT_TOKEN[:15])
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/getMe"
+
+r = requests.get(url)
+
+print("Status:", r.status_code)
+print("Response:", r.text)
