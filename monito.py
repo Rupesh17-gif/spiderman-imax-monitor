ceb0b5258632ea56fc15c54c12217b4fca9c9bd2
Rupesh17-gif <krishnarupesh4@gmail.com>
2026-07-27 08:42:20 +0530

    Enhance BOT_TOKEN logging in monito.py
    Updated print statements to show length and first 10 characters of BOT_TOKEN.

--- monito.py
+++ monito.py
@@ -1,15 +1,8 @@
-import requests
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-print("BOT_TOKEN =", BOT_TOKEN)
-print("CHAT_ID =", CHAT_ID)
-
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/getMe"
-
-r = requests.get(url)
-
-print("Status:", r.status_code)
-print("Response:", r.text)
+print("BOT_TOKEN Length:", len(BOT_TOKEN))
+print("BOT_TOKEN First 10 Chars:", BOT_TOKEN[:10])
+print("CHAT_ID:", CHAT_ID)
